772f7b6255b826a3c2291786da784cc998ebe772
Jyothis P <jyothisp52@gmail.com>
2021-09-09 16:56:29 +0530

    Add previous assignments

--- Program.cs
+++ Program.cs
@@ -11,8 +11,50 @@ namespace ConsoleApp1
         
         static void Main(string[] args)
         {
+            demonstrateIfElse();
+            demonstrateSwitchCase();
             printPattern(20);
             Console.ReadKey();
+        }
+
+        private static void demonstrateSwitchCase()
+        {
+            int x = 5;
+
+            switch (x)
+            {
+                case 1:
+                    Console.WriteLine("x = 1");
+                    break;
+                case 2:
+                    Console.WriteLine("x = 2");
+                    break;
+                case 4:
+                case 5:
+                    Console.WriteLine("x = 4 or x = 5");
+                    break;
+                default:
+                    Console.WriteLine("x > 5");
+                    break;
+            }
+        }
+
+        private static void demonstrateIfElse()
+        {
+            int i = 20, j = 20;
+
+            if (i > j)
+            {
+                Console.WriteLine("i is greater than j");
+            }
+            else if (i < j)
+            {
+                Console.WriteLine("i is less than j");
+            }
+            else
+            {
+                Console.WriteLine("i is equal to j");
+            }
         }
 
         private static void printPattern(int v)
