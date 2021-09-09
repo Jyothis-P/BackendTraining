a62fa1efe5c5a91ac2548891f264407f64d3e140
Jyothis P <jyothisp52@gmail.com>
2021-09-09 16:51:32 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            printPattern(20);
+            Console.ReadKey();
+        }
+
+        private static void printPattern(int v)
+        {
+            for (int i = v; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+        }
+    }
+}
