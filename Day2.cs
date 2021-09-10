a122092a70183332e9b5cc2511804ab6b8865462
Jyothis P <jyothisp52@gmail.com>
2021-09-10 17:52:45 +0530

    Day 2 over.

--- Day2.cs
+++ Day2.cs
@@ -18,7 +18,45 @@ namespace ConsoleApp1
             //inputHandling();
             parseToInt();
             unboxing();
-            persistentAssignment();
+            //persistentAssignment();
+            multiDimensionalArrays();
+            searchInArray();
+            distinctArray();
+        }
+
+        private void distinctArray()
+        {
+            Console.WriteLine("\n\nDistinct Element Array.");
+            int[] nums = {1, 2, 1, 2, 1, 1, 1, 2, 2, 1};
+            Console.WriteLine(String.Join(", ", nums.Distinct()));
+        }
+
+        private void searchInArray()
+        {
+            Console.WriteLine("\n\nSearching, Counting, Printing and Merging Arrays.");
+            string[] names = { "Steve", "Bill", "Bill Gates", "Ravi", "Mohan", "Salman", "Boski" };
+            var stringToFind = "Bill";
+
+            // Note. Returns the first Element alone.
+            //element == stringToFind can be replaced with any condition.
+            var result = Array.Find(names, element => element == stringToFind);
+
+            string[] results = Array.FindAll(names, element => element.StartsWith("B"));
+            string[] resultsWithS = Array.FindAll(names, el => el.StartsWith("S"));
+
+            Console.WriteLine(result);
+            Console.WriteLine(String.Join(", ", results));
+            Console.WriteLine("Array length: " + results.Count());
+            Console.WriteLine("Words with B and S: " + String.Join(", ", results.Union(resultsWithS)));
+
+
+        }
+
+        private void multiDimensionalArrays()
+        {
+            Console.WriteLine("\n\nMultidimensional Arrays.");
+            int[,] matrix = new int[2, 3];
+            Console.WriteLine(matrix);
         }
 
         private void persistentAssignment()
@@ -30,10 +68,6 @@ namespace ConsoleApp1
                 string input = Console.ReadLine();
                 try
                 {
-                    if (input == "q")
-                    {
-                        break;
-                    }
                     Console.WriteLine(assign(Convert.ToInt32(input)));
                     break;
                 }
